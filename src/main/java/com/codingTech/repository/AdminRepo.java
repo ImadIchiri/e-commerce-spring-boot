26acbecb62b476a5ef908b7a1035601abb0fbfff
mariam-01 <mariam.larhlid2001@gmail.com>
2023-09-08 12:46:03 +0100

    admin and user updated

--- src/main/java/com/codingTech/repository/AdminRepo.java
+++ src/main/java/com/codingTech/repository/AdminRepo.java
@@ -24,34 +24,46 @@ public class AdminRepo {
 	// Méthode pour ajouter une nouvelle catégorie
     public void addCategory(Categorie categorie) {
     	em.persist(categorie);
+		em.close();
+
     }
     
     // Méthode pour mettre à jour une catégorie
     public  void updateCategory(Categorie categorie) {
     	em.merge(categorie);
+		em.close();
+
     }
     
     // Méthode pour supprimer une catégorie
     public  void deleteCategory(int categoryId) {
     	Categorie categoryToDelete = em.find(Categorie.class, categoryId);
     	em.remove(categoryToDelete);
+		em.close();
+
     	
     }
 
     //Méthode pour ajouter un produit
     public  void addProduct(Produit produit) {
     	em.persist(produit);
+		em.close();
+
     }
 
     // Méthode pour mettre à jour un produit
     public  void updateProduct(Produit produit) {
     	em.merge(produit);
+		em.close();
+
     }
 
     // Méthode pour supprimer un produit
     public  void deleteProduct(int productId) {
     	Produit produitToDelete = em.find(Produit.class, productId);
     	em.remove(produitToDelete);
+		em.close();
+
     }
 
     // Méthode pour mettre à jour la quantité d'un produit
@@ -62,17 +74,21 @@ public class AdminRepo {
     	
     	produit.setQuantityDispo(produit.getQuantityDispo() - quantity);	
     	em.merge(produit);
+		em.close();
+
     }
 
     // Méthode pour recevoire un produit par son ID
     public  Optional<Produit> getProductById(int productId) {
     	Produit produitById =  em.find(Produit.class,productId);
+		em.close();
     	return Optional.ofNullable(produitById);
     }
 
     // Méthode pour récupérer la liste des commandes passées par les utilisateurs (simplifié ici)
     public  List<Commande> getAllOrders() {
     	List<Commande> listCommande = em.createQuery("from commande").getResultList();
+		em.close();
     	return listCommande;
     }
 
--- src/main/java/com/codingTech/repository/UserRepo.java
+++ src/main/java/com/codingTech/repository/UserRepo.java
@@ -11,51 +11,83 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UserRepo {
 	
 	@PersistenceContext
+
 	EntityManager em;
 	
 	public Optional<User> userLogin(String email, String password) {
 		Query query = em.createQuery("FROM User WHERE where email = :email AND password = :password");
 		query.setParameter("email", email);
-		query.setParameter("password", password);
-		
+		query.setParameter("password", password);	
 		Optional<User> optUser =(Optional<User>) query.getSingleResult();
-		
-		
+		em.close();
 		return optUser;
 	}
 	
-	public static Optional<Role> getRoleById(int id) {
-		return null;
+	public Optional<Role> getRoleById(int id) {
+		
+		Role r = null;
+		r= em.find(Role.class, id);
+		em.close();
+		return Optional.ofNullable(r);
 	}
 	
-	public static Optional<User> getUserById(int id) {
-		return null;
+	public Optional<User> getUserById(int id) {
+		User u = null;
+		u = em.find(User.class, id);
+		em.close();
+		return Optional.ofNullable(u);
 	}
 	
-	public static Optional<Categorie> getCategorieById(int id) {
-		return null;
+	public Optional<Categorie> getCategorieById(int id) {
+		Categorie c = null;
+		c = em.find(Categorie.class, id);
+		em.close();
+		return Optional.ofNullable(c);
 	}
 	
-	public static Optional<Categorie> getCategorieByName(String categorieName) {
-		return null;
+	public Optional<Categorie> getCategorieByName(String categorieName) {
+		Query q = em.createQuery("select c from Categorie c where c.categorie = :categorieNom");
+		q.setParameter("categorieNom", categorieName);
+		Categorie categorie = (Categorie) q.getSingleResult();
+		em.close();
+		return Optional.ofNullable(categorie);
 	}
 	
-	public static List<Categorie> getAllCategories() {
-		return null;
+	public List<Categorie> getAllCategories() {
+		Query q = em.createQuery("from Categorie");
+		List <Categorie> listC = q.getResultList();
+		em.close();
+		return listC;
 	}
 	
-	public static List<Produit> getAllProducts() {
-		return null;
+	public List<Produit> getAllProducts() {
+		Query q = em.createQuery("from Produit");
+		List <Produit> listP = q.getResultList();
+		em.close();
+		return listP;
 	}
 
-	public static List<Produit> getProductsByCategorie(String categorieName) {
-		return null;
+	public List<Produit> getProductsByCategorie(String categorieName) {
+		Query q = em.createQuery("select p from Produit p where p.categorie = :produitCategorie");
+		q.setParameter("produitCategorie", categorieName);
+		List<Produit> ListP = q.getResultList();
+
+		em.close();
+		return ListP;
 	}
 	
-	public static List<Produit> getProductsByName(String productName) {
-		return null;
+	public List<Produit> getProductsByName(String productName) {
+		Query q = em.createQuery("select p from Produit p where p.nom = :produitNom");
+		q.setParameter("produitNom", productName);
+		List<Produit> ListP = q.getResultList();
+
+		em.close();
+		return ListP;
 	}
 }
