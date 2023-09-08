d68aaf674cd6563bf09f7c2cf191310fc3b2a1c3
mariam-01 <mariam.larhlid2001@gmail.com>
2023-09-08 22:01:17 +0100

    adding service classes

--- src/main/java/com/codingTech/service/ClientService.java
+++ src/main/java/com/codingTech/service/ClientService.java
@@ -3,26 +3,30 @@ package com.codingTech.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.codingTech.model.LignePanier;
 import com.codingTech.model.Panier;
 import com.codingTech.model.Produit;
 
+@Service
 public class ClientService implements interClientService{
-
-	public Optional<LignePanier> addProductToPanier(Panier panier, Produit produit, int quantite) {
-		LignePanier lPanier = null;
-		
-		
-		if (produit.getQuantityDispo() >= quantite) {
-			lPanier = new LignePanier(produit, panier, quantite);
+	@Autowired
+	interClientService service;
+	
+		public Optional<LignePanier> addProductToPanier(Panier panier, Produit produit, int quantite) {
+			LignePanier lPanier = null;
+			if (produit.getQuantityDispo() >= quantite) {
+				lPanier = new LignePanier(produit, panier, quantite);
+			}
+			return Optional.ofNullable(lPanier);
 		}
-		
-		return Optional.ofNullable(lPanier);
-	}
-	public void removeProduitFromPanier(int idlLignePanier) {
-	    List<LignePanier> listLignePanier = Panier.getListLignePanier();
-	    
-	    listLignePanier.removeIf(ligne -> ligne.getId() == idlLignePanier);
-	}
+	
+		public void removeProduitFromPanier(int idlLignePanier) {
+		    List<LignePanier> listLignePanier = Panier.getListLignePanier();
+		    
+		    listLignePanier.removeIf(ligne -> ligne.getId() == idlLignePanier);
+		}
 	
 }
